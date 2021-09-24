a44d7f0fdd5ec2a16386b7993cc698449cbc3149
np03a180030 <aishugrg0099@gmail.com>
2021-09-24 15:24:53 +0545

    Third push

--- src/main/java/Question1.java
+++ src/main/java/Question1.java
@@ -14,23 +14,21 @@ public class Question1 {
         List<Integer> evenNumbers = new ArrayList<>();
         List<Integer> primeNumbers = new ArrayList<>();
 
-        Consumer< List<Integer> > lists = list->{
-            list.stream()
-                    .filter(value ->value %2 ==0)
-                    .forEach(evenNumbers::add);
-
-            list.stream()
-                    .filter(value ->value %2 !=0)
-                    .forEach(oddNumbers::add);
-
-            list.stream()
-                    .filter(e->IntStream.range(2,e).noneMatch(v-> e % v == 0 ))
-                    .filter(e-> e!= 1)
-                    .forEach(primeNumbers::add);
-        };
+        Consumer< List<Integer> > oddList = list-> list.stream()
+                                                   .filter(value ->value %2 !=0)
+                                                    .forEach(oddNumbers::add);
+        Consumer< List<Integer> > evenList = list->list.stream()
+                                                .filter(value ->value %2 ==0)
+                                                .forEach(evenNumbers::add);
+        Consumer< List<Integer> > primeList = list->list.stream()
+                                                   .filter(e->IntStream.range(2,e).noneMatch(v-> e % v == 0 ))
+                                                   .filter(e-> e!= 1)
+                                                   .forEach(primeNumbers::add);
 
         Consumer< List<Integer> > printLists = System.out::println;
-        lists.accept(numbers);
+        oddList.accept(numbers);
+        evenList.accept(numbers);
+        primeList.accept(numbers);
 
         //displaying odd number list.
         System.out.print("Odd numbers from 1 to 50 are as follows: ");
